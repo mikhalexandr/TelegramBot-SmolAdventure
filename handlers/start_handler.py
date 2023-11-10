ac91cc47a4bda25e521a362db6070aed95985cd4
Alexandr <mikhalchenkov22@gmail.com>
2023-11-10 13:40:41 +0300

    meow

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,11 +1,12 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
+import consts
 
 router = Router()
 
 
 @router.message(Command("start"))
 async def start(msg: Message):
-    await msg.answer("Привет")
+    await msg.answer(consts.START_MESSAGE)
 
