97f952ae849d17e72b8dd2f66c58acc96f26c6bd
mathusha2023 <bazooka2008@inbox.ru>
2023-11-10 13:14:31 +0300

    aaa

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+import logging
+from handlers import start_handler
+import config
+
+
+async def main():
+    bot = Bot(token=config.BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
+
+
 
