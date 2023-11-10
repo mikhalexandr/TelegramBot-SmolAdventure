61b037927215fe7b4135eff3fb38eab150991481
mathusha2023 <bazooka2008@inbox.ru>
2023-11-10 15:54:22 +0300

    aaa

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F, types
 from aiogram.types import Message
 from aiogram.filters import Command
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardMarkup
+import keyboards
 import consts
 
 router = Router()
@@ -9,14 +9,17 @@ router = Router()
 
 @router.message(Command("start"))
 async def start(msg: Message):
-    builder = ReplyKeyboardBuilder()
-    builder.row(
-        types.KeyboardButton(text="История Смоленска"),
-        types.KeyboardButton(text="Квест"))
-    global page
-    page = 0
-    await msg.answer(consts.START_MESSAGE,
-                         reply_markup=builder.as_markup(resize_keyboard=True))
+    await msg.answer(consts.START_MESSAGE, reply_markup=keyboards.create_start_kb())
+
+
+@router.message(F.text == "История Смоленска")
+async def history(msg: Message):
+    await msg.answer("Здесь будет история Смоленска", reply_markup=keyboards.ReplyKeyboardRemove())
+
+
+@router.message(F.text == "Квесты")
+async def quests(msg: Message):
+    await msg.answer("Здесь будут квесты", reply_markup=keyboards.ReplyKeyboardRemove())
 
 
 @router.message(Command("help"))
@@ -24,28 +27,3 @@ async def help(msg: Message):
     await msg.answer(consts.get_help_message())
 
 
-@router.message(F.text == "История Смоленска")
-async def smolhist(message: types.Message, page=0):
-    builder = ReplyKeyboardBuilder()
-    builder.row(
-        types.KeyboardButton(text="1"), types.KeyboardButton(text="2")
-    )
-    builder.row(
-        types.KeyboardButton(text="3"), types.KeyboardButton(text="4"),
-        types.KeyboardButton(text="5"))
-    builder.row(types.KeyboardButton(text="<="),
-                types.KeyboardButton(text="=>"))
-    await message.reply("Выберите материал", reply_markup=builder.as_markup(resize_keyboard=True))
-
-
-@router.message(F.text == "Квест")
-async def quest(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    builder.row(
-        types.KeyboardButton(text="Экскурсия"), types.KeyboardButton(text=''))
-    await message.reply("[текст экскурсии]", reply_markup=types.ReplyKeyboardRemove())
-# @dp.message(Command("special_buttons"))
-# async def cmd_special_buttons(message: types.Message):#     builder = ReplyKeyboardBuilder()
-#     builder.row(#         types.KeyboardButton(text="Запросить геолокацию", request_location=True),
-#         types.KeyboardButton(text="Запросить контакт", request_contact=True))#     await message.answer(
-#         "Выберите действие:",#         reply_markup=builder.as_markup(resize_keyboard=True))
--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,6 @@
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 
+
+def create_start_kb():
+    kb = [KeyboardButton(text="История Смоленска"), KeyboardButton(text="Квесты")]
+    return ReplyKeyboardMarkup(keyboard=[kb], resize_keyboard=True)
