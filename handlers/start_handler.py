79f1c06fa410be325fccda18af2606a690c056e6
Alexandr <mikhalchenkov22@gmail.com>
2023-11-11 08:53:31 +0300

    мяу

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -30,6 +30,11 @@ async def quests(msg: Message, state: FSMContext):
     await state.set_state(states.QuestsStates.setting_quest)
 
 
+@router.message(F.text == "Поддержать автора")
+async def payment(msg: Message):
+    await msg.answer("Нажмите на кнопку ниже, чтобы поддержать автора", reply_markup=keyboards.payments_kb())
+
+
 @router.message(Command("info"))
 async def help_msg(msg: Message):
     await msg.answer(consts.INFO_MASSAGE)
