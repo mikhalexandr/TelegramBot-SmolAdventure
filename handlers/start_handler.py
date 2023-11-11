a272f12517b971290a9a2a36cfb70282f84e370b
mathusha2023 <bazooka2008@inbox.ru>
2023-11-11 03:01:27 +0300

    aaa

---
+++ handlers/start_handler.py
@@ -0,0 +1,40 @@
+# обработчики базовых команд и неопознанных сообщений
+
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
+import keyboards
+import consts
+import states
+
+router = Router()
+
+
+@router.message(Command("start"))
+async def start(msg: Message):
+    await msg.answer(consts.START_MESSAGE, reply_markup=keyboards.create_start_kb())
+
+
+@router.message(F.text == "История Смоленска")
+async def history(msg: Message, state: FSMContext):
+    await msg.answer("Пожалуйста, выбери предмет изучения", reply_markup=keyboards.setting_history_kb())
+    await state.set_state(states.HistoryStates.setting_history)
+
+
+@router.message(F.text == "Квесты")
+async def quests(msg: Message, state: FSMContext):
+    await msg.answer("Пожалуйста, выбери квест", reply_markup=keyboards.setting_quest_kb())
+    await state.set_state(states.QuestsStates.setting_quest)
+
+
+@router.message(Command("help"))
+async def help_msg(msg: Message):
+    await msg.answer(consts.get_help_message())
+
+
+@router.message()
+async def unknown_message(msg: Message):
+    await msg.answer("Нет такого варианта!")
+
+
---
+++ main.py
@@ -0,0 +1,25 @@
+
+# основной файл программы, из которого запускается бот
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+import logging
+from handlers import start_handler, quests_handler, history_handler
+import config
+
+
+async def main():
+    bot = Bot(token=config.BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_routers(quests_handler.router, history_handler.router, start_handler.router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
+
+
+
